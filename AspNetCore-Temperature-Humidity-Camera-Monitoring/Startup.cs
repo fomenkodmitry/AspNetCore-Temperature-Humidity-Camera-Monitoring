7bd98d56ad056b97191087491500958fe498cb6d
Dmitriy Fomenko <blackreaper200@gmail.com>
2020-03-07 23:28:41 +0300

    upd

--- AspNetCore-Temperature-Humidity-Camera-Monitoring/Startup.cs
+++ AspNetCore-Temperature-Humidity-Camera-Monitoring/Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -40,6 +41,15 @@ namespace AspNetCore_Temperature_Humidity_Camera_Monitoring
 
             app.UseRouting();
 
+            // using Microsoft.AspNetCore.HttpOverrides;
+
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+            });
+
+            app.UseAuthentication();
+            
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
