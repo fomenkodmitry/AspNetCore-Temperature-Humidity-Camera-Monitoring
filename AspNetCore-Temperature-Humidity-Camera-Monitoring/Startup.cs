e8f3db086b24878f0aa0d8effc62e450659b7315
Dmitriy Fomenko <blackreaper200@gmail.com>
2020-03-07 23:22:18 +0300

    upd

--- AspNetCore-Temperature-Humidity-Camera-Monitoring/Startup.cs
+++ AspNetCore-Temperature-Humidity-Camera-Monitoring/Startup.cs
@@ -11,7 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace AspNetCore_Temprature_Humidity_Camera_Monitoring
+namespace AspNetCore_Temperature_Humidity_Camera_Monitoring
 {
     public class Startup
     {
