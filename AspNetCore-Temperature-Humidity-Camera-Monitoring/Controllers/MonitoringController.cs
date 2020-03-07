e5259fd9da6e0eb3ef5568e10bd551aba169509a
Dmitriy Fomenko <blackreaper200@gmail.com>
2020-03-07 23:05:11 +0300

    inital

---
+++ AspNetCore-Temperature-Humidity-Camera-Monitoring/Controllers/MonitoringController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Iot.Device.DHTxx;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCore_Temperature_Humidity_Camera_Monitoring.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MonitoringController : ControllerBase
+    {
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            using var dht = new Dht22(4);
+            Console.WriteLine($"Temperature: {dht.Temperature.Celsius:0.0}°C, Humidity: {dht.Humidity:0.0}%");
+            return Ok(new
+            {
+                Temperature = dht.Temperature.Celsius.ToString("0.0"),
+                Humidity = dht.Humidity.ToString("0.0")
+            });
+        }
+    }
+}
