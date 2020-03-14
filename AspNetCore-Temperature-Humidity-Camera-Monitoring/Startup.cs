9e9d3a714009b7fc06e1cd59346617594e0076bb
Dmitriy Fomenko <blackreaper200@gmail.com>
2020-03-14 16:48:18 +0300

    add task run dht

--- AspNetCore-Temperature-Humidity-Camera-Monitoring/Startup.cs
+++ AspNetCore-Temperature-Humidity-Camera-Monitoring/Startup.cs
@@ -1,7 +1,11 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using Iot.Device.DHTxx;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpOverrides;
@@ -37,6 +41,26 @@ namespace AspNetCore_Temperature_Humidity_Camera_Monitoring
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseWebSockets();
+            app.Use(async (context, next) =>
+            {
+                if (context.Request.Path == "/ws")
+                {
+                    if (context.WebSockets.IsWebSocketRequest)
+                    {
+                        await context.WebSockets.AcceptWebSocketAsync();
+                    }
+                    else
+                    {
+                        context.Response.StatusCode = 400;
+                    }
+                }
+                else
+                {
+                    await next();
+                }
+            });
+
             app.UseHttpsRedirection();
 
             app.UseRouting();
@@ -49,10 +73,51 @@ namespace AspNetCore_Temperature_Humidity_Camera_Monitoring
             });
 
             app.UseAuthentication();
-            
-            app.UseAuthorization();
 
+            app.UseAuthorization();
+            Task.Factory.StartNew(
+                _runDht,
+                CancellationToken.None,
+                TaskCreationOptions.LongRunning,
+                TaskScheduler.Default
+            ).Unwrap();
             app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+        }
+
+        private async Task _runDht()
+        {
+            var webSocket = new ClientWebSocket();
+            // var ws = new ClientWebSocket();
+            // var uri = new Uri("wss://ws.binaryws.com/websockets/v3");
+ 
+            // await ws.ConnectAsync(uri, CancellationToken.None);
+            
+            using var dht = new Dht22(4);
+            while (true)
+            {
+                // Try to read the temperature.
+                var temp = dht.Temperature;
+                if (!dht.IsLastReadSuccessful) continue;
+
+                // Try to read the humidity.
+                var humidity = dht.Humidity;
+                if (!dht.IsLastReadSuccessful) continue;
+
+                var res = $"Temperature: {temp.Celsius:0.0} °C, 'Humidity: {humidity:0.0} %";
+                Console.WriteLine(res);
+                await webSocket.SendAsync(
+                    buffer: new ArraySegment<byte>(
+                        array: Encoding.ASCII.GetBytes(res),
+                        offset: 0,
+                        count: res.Length
+                    ),
+                    messageType: WebSocketMessageType.Text,
+                    endOfMessage: true,
+                    cancellationToken: CancellationToken.None
+                );
+                await Task.Delay(2000);
+            }
+            
         }
     }
 }
