5d6c253f769601e9a82be4eeee1048b3e3f273d2
Дмитрий Фоменко <dfomenko@mmtr.ru>
2020-09-30 22:37:06 +0300

    stream webcam

--- AspNetCore-Temperature-Humidity-Camera-Monitoring/Startup.cs
+++ AspNetCore-Temperature-Humidity-Camera-Monitoring/Startup.cs
@@ -102,8 +102,7 @@ namespace AspNetCore_Temperature_Humidity_Camera_Monitoring
                 var humidity = dht.Humidity;
                 if (!dht.IsLastReadSuccessful) continue;
                 
-                var res = $"Temperature: {temp.DegreesCelsius:0.0}°C, Humidity: {humidity:0.0}%";
-                // var res = RandomNumberGenerator.GetInt32(1,60).ToString();
+                var res = $"Temperature: {temp.MillidegreesCelsius:0.0}°C, Humidity: {humidity:0.0}%";
                 Console.WriteLine(res);
                 await webSocket.SendAsync(
                     buffer: new ArraySegment<byte>(
--- AspNetCore-Temperature-Humidity-Camera-Monitoring/wwwroot/js/stream.js
+++ AspNetCore-Temperature-Humidity-Camera-Monitoring/wwwroot/js/stream.js
@@ -1,15 +1,14 @@
 (function() {
-    var getWebSocketMessages = function(onMessageReceived)
-    {
-        var url = `ws://${location.host}/ws`
+    const getWebSocketMessages = function (onMessageReceived) {
+        const url = `ws://${location.host}/ws`;
         console.log('url is: ' + url);
 
-        var webSocket = new WebSocket(url);
+        const webSocket = new WebSocket(url);
 
         webSocket.onmessage = onMessageReceived;
     };
 
-    var ulElement = document.getElementById('stream');
+    const ulElement = document.getElementById('stream');
 
     getWebSocketMessages(function (message) {
         ulElement.innerHTML = `<li>${message.data}</li>`
